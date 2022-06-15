d8b88452a61e1d816dde59aeec1dbbc2425d9529
Elijah Noble <elijahnobledev@gmail.com>
2022-06-14 22:01:20 -0700

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,36 @@
-// create your App component here
+import React, { useState, useEffect } from "react"
+
+function App() {
+
+    const [image, setImage] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    const url = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(() => {
+        if (!loading) setLoading(true);
+        const timeoutID = setTimeout(() => {
+
+            fetch(url)
+            .then((e) => e.json())
+            .then((data) => {
+                setImage(data.message);
+                setLoading(false);
+            })
+        }, 2000)
+        
+        return function cleanup() {
+            clearTimeout(timeoutID);      
+            };
+    },[image]);
+
+
+
+    return (
+        <div>
+            {loading ? (<p>"Loading..."</p>) : (<img src={image} alt="A Random Dog" />)}
+        </div>
+    )
+}
+
+export default App
